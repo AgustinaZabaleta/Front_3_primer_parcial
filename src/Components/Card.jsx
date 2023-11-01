a222f71ee34012aa80424b3058688ab8f08fe421
Agustina Zabaleta <agustina.zabaleta@oracle.com>
2023-11-01 18:58:53 -0300

    sibiendo parcial primera parte

---
+++ src/Components/Card.jsx
@@ -0,0 +1,19 @@
+
+import React from "react";
+
+
+const Card = () => {
+  return (
+    <React.Fragment>
+  <div>Hola, soy una tarjeta</div>;
+  
+      <button>Añadir</button>
+      </React.Fragment>
+      )
+};
+
+
+export default Card;
+
+
+
